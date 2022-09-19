c26cdf1616734d2110904781fd4568b23dd52cb0
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-09-19 11:12:45 +0100

    WIP

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { ROUTES } from '../lib/routes'
 
 const ErrorPage: React.FC = () => (
-  <>ErrorPage</>
+  <div className="mt-6 p-4 max-w-sm mx-auto text-center">
+    <p className="mb-1">There was an error.</p>
+    <a href={ROUTES.root} className="text-blue-800 underline">
+      Return to home
+    </a>
+  </div>
 )
 
 export default ErrorPage
