9fc7a846e560d7004be2e3177a88b9c77ebd35fa
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-09-19 11:49:21 +0100

    WIP

--- src/examples/use-effect-clean-up-behaviour.tsx
+++ src/examples/use-effect-clean-up-behaviour.tsx
@@ -12,10 +12,10 @@ const UseEffectCleanUpBehaviour: React.FC = () => {
       // React will wait until all clean up functions finish executing before
       // it processes the next set of effects.
       // So, in this case, it'll wait for the lengthy useEffect1CleanUp to run
-      // to completion before it processes the useEffet2CleanUp function.
+      // to completion before it processes the useEffcet2CleanUp function.
       return function useEffect1CleanUp() {
         for (let i = 0; i < 4000000000; i++) {
-          //
+          // Simulate expensive operation
         }
         console.log('Inside useEffect1CleanUp')
       }
