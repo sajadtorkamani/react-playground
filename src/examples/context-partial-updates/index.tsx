1ca3629eca13d56d2ca600c9e8abc530541509f5
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-10-02 09:42:11 +0100

    WIP

--- src/examples/context-partial-updates/index.tsx
+++ src/examples/context-partial-updates/index.tsx
@@ -8,6 +8,12 @@ import ThemeSettings from './components/ThemeSettings'
 const ContextPartialUpdates: React.FC = () => {
   return (
     <AppSettingsProvider>
+      <p>
+        Updating the `theme` from the context triggers a re-render of the
+        `DateSettings` component too even though `DateSettings` doesn't use
+        `theme`.
+      </p>
+
       <div className="mb-3">
         <ThemeSettings />
       </div>
